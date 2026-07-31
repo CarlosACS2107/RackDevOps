7aae231c2ffbb5e8483cdd7a8c8533c71bb358af
Carlos Alberto Cabrera Solis <carmin21tatito@gmail.com>
2026-07-31 17:02:04 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
+const mysql = require("mysql2");
+
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,   // <-- IMPORTANTE: debe ser DB_USERNAME
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
 });
+
+module.exports = connection.promise();
